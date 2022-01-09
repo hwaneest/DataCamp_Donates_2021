bff9cfb53d94a48d1578b59531e0fb8a7be8c89b
taekkim <taekkim@student.42seoul.kr>
2022-01-09 16:56:40 +0900

    perf: pre-calculate
    #10

--- app/app.py
+++ app/app.py
@@ -7,6 +7,7 @@ from matplotlib import font_manager
 import seaborn as sns
 import os
 
+# 한글폰트
 if os.name == 'posix':
     font_manager.fontManager.addfont('font/NanumGothicCoding-Regular.ttf')
     plt.rc("font", family='NanumGothicCoding')
@@ -21,19 +22,20 @@ add_selectbox = st.sidebar.selectbox(
     ("선택", "오미크론", "주식", '코로나와 소득수준', '사회적 거리두기')
 )
 
-# Data
+# csv files
 @st.experimental_memo
 def read_df(path):
     return pd.read_csv(path)
 
 df = read_df('./data/trends-extend.csv')
 df_covid = read_df('./data/covid_data_.csv')
+today = df.iloc[0, 0]
 
-# Body
+# Dashboard
 st.title('How serious is covid now?')
 
 st.header('오늘 코로나19')
-st.caption(f'updated: {df.iloc[0, 0]}')
+st.caption(f'updated: {today}')
 
 st.subheader('전국')
 col1, col2, col3 = st.columns(3)
@@ -112,19 +114,19 @@ st.bar_chart(df_seoul_bar[::30])
 # x= df_covid["확진일"].value_counts().sort_index()
 # x = df['전국 기준일'].apply(lambda x: x.replace('.', '-'))
 
-fig, ax = plt.subplots(figsize=(10,8))
+# fig, ax = plt.subplots(figsize=(10,8))
 # sns.lineplot( x.index, x.values ,color = "r", ax = ax)
-sns.lineplot( df['서울시 기준일'], df['서울시 추가 확진'] ,color = "r", ax = ax)
+# sns.lineplot( df['서울시 기준일'], df['서울시 추가 확진'] ,color = "r", ax = ax)
 
-ax.set_title("기간별 확진자 증가 추이")
-ax.set_ylabel("확진자수")
-st.pyplot(fig)
+# ax.set_title("기간별 확진자 증가 추이")
+# ax.set_ylabel("확진자수")
+# st.pyplot(fig)
 
 # Plotly
-# x = df['전국 기준일'].apply(lambda x: x.replace('.', '-'))
-# fig = px.line(df[::-1], x=x[::-1], y=['전국 추가 확진','서울시 추가 확진'],title=f'코로나19 확진자 발생 통계 (updated: {df.iloc[0, 0]})', labels={'x':'날짜', 'value':'확진자 발생', 'variable':'지역'})
-# st.plotly_chart(fig, use_container_width=True)
-# st.write(f'20.02.05 ~ {df.iloc[0, 0]} 까지의 코로나 확진자수를 나타낸 라인 그래프입니다.20년도에는 집단감염원인이 그래프의 변동성을 키웠고 21년도에 들어서면서 집단감염이 아닌 지역감염으로 번짐에 따라 확진자수가 가파르게 증가하는 것을 볼 수 있습니다.')
+x = df['전국 기준일'].apply(lambda x: x.replace('.', '-'))
+fig = px.line(df[::-1], x=x[::-1], y=['전국 추가 확진','서울시 추가 확진'],title=f'코로나19 확진자 발생 통계 (updated: {today})', labels={'x':'날짜', 'value':'확진자 발생', 'variable':'지역'})
+st.plotly_chart(fig, use_container_width=True)
+st.write(f'20.02.05 ~ {today} 까지의 코로나 확진자수를 나타낸 라인 그래프입니다.20년도에는 집단감염원인이 그래프의 변동성을 키웠고 21년도에 들어서면서 집단감염이 아닌 지역감염으로 번짐에 따라 확진자수가 가파르게 증가하는 것을 볼 수 있습니다.')
 
 t = df_covid["접촉력"].value_counts().sort_values().tail(10)
 st.subheader("접촉력 파악")
@@ -135,19 +137,19 @@ plt.xticks(rotation = 60)
 st.pyplot(fig)
 st.write('전체 기간 확진자 접촉력에 대해 파악한 막대 그래프입니다.최근 지역감염이 급격하게 증가하며 "기타확진자 접촉", "감염경로조사중" 등 원인을 파악하기 어려운 경우가 다분한것으로 보입니다.')
 
-df_covid["퇴원"] = df_covid["퇴원현황"].str.contains("퇴원", na=False)
-df_covid["사망"] = df_covid["퇴원현황"].str.contains("사망", na=False)
-
-퇴원_value = df_covid["퇴원현황"].value_counts()
-
-st.subheader("퇴원자 사망자 비교")
+st.subheader(f"퇴원자 사망자 비교 (Updated: {today}")
 
 fig , ax  = plt.subplots(figsize = (13,10))
 
+total_alive = df['전국 퇴원'][0]
+total_death = df['전국 사망'][0]
+
+per_alive = total_alive * 100 / (total_alive + total_death)
+per_death = total_death * 100 / (total_alive + total_death)
+
 wedgeprops={'width': 0.6, 'edgecolor': 'w', 'linewidth': 5}
 colors = ["green","red"]
-
-ax.pie(x = 퇴원_value.values, labels = 퇴원_value.index, autopct='%.1f%%'
+ax.pie(x = (per_alive, per_death), labels = ('퇴원', '사망'), autopct='%.1f%%'
     ,  startangle=260, counterclock=False,  wedgeprops=wedgeprops, textprops ={'size' :20}, colors = colors)
 st.pyplot(fig)
 st.write(' 확진자중 퇴원과 사망한 확진자의 분포를 나타낸 파이차트 입니다. 파이차트만으로는 확진자 중 사망자가 1%로 적은걸 알 수 있습니다. 하지만 이는 잘못된 결론이라고 판단이 듭니다.코로나의 치명률은 연령대에 다르기 때문입니다. 현재 데이터가 부족해 분석을 진행하지는 못하지만 조사해본 봐로는 20대의 경우 치명률이 0.01%이고 30대 0.04%, 40대 0.06%, 50대 0.26%로 1% 미만이지만 코로나 치명률은 고령층으로 갈수록 크게 뜁니다. 60대는 1.05%, 70대는 5.57%, 80대 이상은 18.69%에 달했다.이와 같이 단순 총 사망자 분포를 통해 코로나가 치명적이지 않다고 단정 지을 수는 없다고 생각합니다.')
